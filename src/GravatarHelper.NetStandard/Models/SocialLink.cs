55427ce6e0f9d3bbc0151e151146eaff9886d649
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2017-01-13 21:51:29 +0530

    Refactoring

--- src/GravatarHelper.NetStandard/Models/SocialLink.cs
+++ src/GravatarHelper.NetStandard/Models/SocialLink.cs
@@ -4,9 +4,15 @@ namespace GravatarHelper.NetStandard.Models
 {
     public class SocialLink
     {
+        /// <summary>
+        /// Social Site Profile Link
+        /// </summary>
         [JsonProperty(PropertyName ="value")]
         public string Url { get; set; }
 
+        /// <summary>
+        /// Social Site Name
+        /// </summary>
         [JsonProperty(PropertyName = "title")]
         public string SiteName { get; set; }
     }
