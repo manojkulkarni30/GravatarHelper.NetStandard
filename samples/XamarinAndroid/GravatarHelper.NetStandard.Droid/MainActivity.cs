230aa254d34fdae6e4ce16a87b0b5b2e81769b2b
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2017-01-15 12:54:38 +0530

    Tested the package in Xamarin Android App

--- samples/XamarinAndroid/GravatarHelper.NetStandard.Droid/MainActivity.cs
+++ samples/XamarinAndroid/GravatarHelper.NetStandard.Droid/MainActivity.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
+﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Graphics;
+using System.Net;
 
 namespace GravatarHelper.NetStandard.Droid
 {
     [Activity(Label = "GravatarHelper.NetStandard.Droid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : BaseActivity
     {
+        private Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
 
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
         protected override int LayoutResource
         {
             get { return Resource.Layout.main; }
         }
-        int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            var clickButton = FindViewById<Button>(Resource.Id.my_button);
-
-            clickButton.Click += (sender, args) =>
-              {
-                  clickButton.Text = string.Format("{0} clicks!", count++);
-              };
-
-            var navigationButton = FindViewById<Button>(Resource.Id.nav_button);
-
-            navigationButton.Click += (sender, args) =>
-              {
-                  var intent = new Intent(this, typeof(SecondActivity));
-                  intent.PutExtra("clicks", count);
-                  StartActivity(intent);
-              };
-
-
-
             SupportActionBar.SetDisplayHomeAsUpEnabled(false);
             SupportActionBar.SetHomeButtonEnabled(false);
+
+            var imageBitmap = GetImageBitmapFromUrl(Gravatar.GetGravatarImageUrl("example@test.com", imageSize: 200));
+            var imageView = FindViewById<ImageView>(Resource.Id.image);
+            imageView.SetImageBitmap(imageBitmap);
+
+            imageBitmap = GetImageBitmapFromUrl(Gravatar.GetGravatarProfileQrCodeImage("example@test.com", imageSize: 200));
+            var qrCodeImageView = FindViewById<ImageView>(Resource.Id.qrcodeimage);
+            qrCodeImageView.SetImageBitmap(imageBitmap);
+
 
         }
     }
